f72abf6ad2e781c42be36016104a1d58be0b0afc
Akshay Darekar <akshaydarekar5252@gmail.com>
2026-01-02 00:51:11 +0530

    Add basic area and average calculation programs

---
+++ basic/Area.java
@@ -0,0 +1,12 @@
+package basic;
+import java.util.*;
+
+public class Area {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        int area = a*a;
+        System.out.println(area);
+    }
+}
--- basic/Conversion.java
+++ basic/Conversion.java
@@ -1,3 +1,4 @@
+package basic;
 import java.util.*;
 public class Conversion {
     public static void main(String args[]){
