e6229c53cf246db9faeabdea0559fbfa5ed4a621
Akshay Darekar <akshaydarekar5252@gmail.com>
2026-01-01 23:37:35 +0530

    conversion of data

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class JavaBasics {
     public static void main(String args[]) {
-        int a = 10;
-        int b = 10;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int sum = a + b;
         System.out.println(sum);
-        
     }
 
 }
