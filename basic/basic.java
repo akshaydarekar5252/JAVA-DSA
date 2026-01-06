9bc59063363c1d41569c16c8fedc44f09bcd30a1
Akshay Darekar <akshaydarekar5252@gmail.com>
2026-01-06 20:10:50 +0530

    basic

--- basic/basic.java
+++ basic/basic.java
@@ -1,24 +1,39 @@
 import java.util.*;
 public class basic {
     public static void main(String args[]) {
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         // int a = sc.nextInt();
         // int b = sc.nextInt();
         // int sum = a + b;
         // System.out.println(sum);
 
-        int age = 23;
-        if (age >= 18) {
-            System.out.println("You can Vote ");
-        }
+//         int age = 23;
+//         if (age >= 18) {
+//             System.out.println(" adult ");
+//         }
 
-        if (age > 13 && age < 18 ) {
-            System.out.println("teenager");
-        }
+//        else if (age >= 13 && age < 18 ) {
+//             System.out.println("teenager");
+//         }
         
-        else{
-            System.out.println("You are child ");
+//         else{
+//             System.out.println("You are child ");
+//         }
+
+        int income = sc.nextInt();
+        int tax = 0;
+        if (income < 500000 ){
+            tax = 0 ;
+            
         }
+        else if (income >=500000 && income < 1000000) {
+            tax = (int) (income * 0.2);  // int is used for converting the double value in the integer value 
+           
+        }else{
+            tax = (int) (income * 0.3);
+           
+        }
+         System.out.println("Your tax is " + tax);
     }
 
 }
---
+++ basic/largestnum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class largestnum {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a >= b){
+            System.out.println("a is largest of 2");
+        }else{
+            System.out.println("b is largest of 2 ");
+        }
+    }
+}
